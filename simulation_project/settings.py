40f9b5cf613b469fb22370307735616d6c1b031b
starkvic <starkvic@github.com>
2025-04-11 07:35:37 +0300

    Managed to change to add static files

--- simulation_project/settings.py
+++ simulation_project/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file___)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/ 'static']
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
